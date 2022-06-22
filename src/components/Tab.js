f88149f646cf1aadbc106d160002c52844c53de7
Umesh Jain <42075232+umeshjain1999@users.noreply.github.com>
2022-06-22 21:28:29 +0530

    initial commit

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,25 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 const Tab = () => {
   return (
-    <div
-      className="tabs"
-      style={{
-        width: "200px",
-        height: "30px",
-        display: "flex",
-        justifyContent: "space-between"
-      }}
-    >
-      <div>
-        <Link to="/">All</Link>
-      </div>
-      <div>
-        <Link to="/active">Active</Link>
-      </div>
-      <div>
-        <Link to="/completed">Completed</Link>
-      </div>
+    <div className="tabs">
+      <NavLink to="/" className="tab">
+        All
+      </NavLink>
+      <NavLink to="/active" className="tab" activeClassName="selected">
+        Active
+      </NavLink>
+      <NavLink to="/completed" className="tab" activeClassName="selected">
+        Completed
+      </NavLink>
     </div>
   );
 };
